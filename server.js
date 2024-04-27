d9fb9e72378a2746a3711b06a611dd754b906a39
Hans0l023 <watts.hans@gmail.com>
2024-04-27 09:39:40 -0500

    first commit

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require('express-ejs-layouts')
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -13,18 +14,24 @@ const static = require("./routes/static")
 /* ***********************
  * Routes
  *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 /* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
+* Local Server Information
+* Values from .env (environment) file
+*************************/
 const port = process.env.PORT
 const host = process.env.HOST
 
 /* ***********************
- * Log statement to confirm server operation
- *************************/
+* Log statement to confirm server operation
+*************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+// Index route
+app.get("/", function(req, res) {res.render('index', {title: 'Home'})})
